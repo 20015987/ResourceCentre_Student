792ae1a4aadcfe597bd5ae163b7ab874e63be4d0
20032405 <20032405@DESKTOP-OB82SS8>
2021-07-23 12:51:00 +0800

    Testing

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -116,7 +116,7 @@ public class ResourceCentreTest {
 		testOutput += String.format("%-10s %-30s %-10s %-10s %-20s\n","CB0012", "HP Chromebook", "Yes", "Win 10");
 
 		assertEquals("Check that ViewAllChromebookList", testOutput, allChromebook);
- 
+  
 	}
  
 	@Test
