84224ba4d1b24fc6516eeb6039e9f91095712af5
20015987 <20015987@Mizuki>
2021-07-23 16:41:56 +0800

    Fixed format problem and finished testRetrieveAllChromebook method.

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -82,9 +82,34 @@ public class ResourceCentreTest {
 		
 	}
 	@Test
-	public void testRetrieveAllChromebook() {
-		//fail("Not yet implemented");
-		// write your code here
+	public void testRetrieveAllChromebook() { // Done by: Ammar
+		
+		// Test if Item list is not null but empty, so that can add a new item
+		assertNotNull("Test if there is valid chromebookList arraylist to add to", chromebookList);
+		
+		//test if the list of chromebook retrieved from the SourceCentre is empty
+		String allChromebook= ResourceCentre.retrieveAllChromebook(chromebookList);
+		String testOutput = "";
+		assertEquals("Check that ViewAllChromebook", testOutput, allChromebook);
+				
+		//Given an empty list, after adding 2 items, test if the size of the list is 2
+		ResourceCentre.addChromebook(chromebookList, cb1);
+		ResourceCentre.addChromebook(chromebookList, cb2);
+		assertEquals("Test if that Chromebook arraylist size is 2?", 2, chromebookList.size());
+		
+		//test if the expected output string same as the list of camcorders retrieved from the SourceCentre
+		allChromebook = ResourceCentre.retrieveAllChromebook(chromebookList);
+		
+		testOutput = String.format("%-10s %-30s %-10s %-10s %-20s\n", cb1.getAssetTag(), cb1.getDescription()
+																	, ResourceCentre.showAvailability(cb1.getIsAvailable())
+																	, cb1.getDueDate(), cb1.getOs());
+		testOutput += String.format("%-10s %-30s %-10s %-10s %-20s\n", cb2.getAssetTag(), cb2.getDescription()
+																	 , ResourceCentre.showAvailability(cb2.getIsAvailable())
+																	 , cb2.getDueDate(), cb2.getOs());
+		
+		
+		assertEquals("Check that ViewAllChromebooklist", testOutput, allChromebook);
+		
 	}
 
 	@Test
